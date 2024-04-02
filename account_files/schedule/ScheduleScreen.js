8e6aa930a10646059a2a453dcb4e71a5ff9f1d14
TJWRii <s1912558@ed.ac.uk>
2024-04-02 17:48:31 +0100

    fix update error

--- account_files/schedule/ScheduleScreen.js
+++ account_files/schedule/ScheduleScreen.js
@@ -5,21 +5,15 @@ import { useAuth } from '../AuthContext';
 const ScheduleScreen = () => {
   const { userInfo } = useAuth();
   const [refreshing, setRefreshing] = useState(false);
+  const [agendaKey, setAgendaKey] = useState('initialKey');
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
-    // Here, you should trigger any necessary data fetching or state updates
-    // that the AgendaScreen needs to refresh its content.
-    // After the data fetching is done, set refreshing to false.
-    setTimeout(() => { // Simulating a network request
-      setRefreshing(false);
-    }, 1500);
+    setAgendaKey(prevKey => prevKey + '1');
+    setRefreshing(false);
   }, []);
 
-  // Pass the refreshing and onRefresh props to the AgendaScreen.
-  // You will need to modify the AgendaScreen component to accept these props
-  // and pass them appropriately to the Agenda component it renders.
-  return <AgendaScreen user_id={userInfo?.id} refreshing={refreshing} onRefresh={onRefresh} />;
+  return <AgendaScreen key={agendaKey} user_id={userInfo?.id} refreshing={refreshing} onRefresh={onRefresh} />;
 };
 
 export default ScheduleScreen;

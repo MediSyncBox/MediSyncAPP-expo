f3bbab34829ceb02a94771cb2b45683c0c065217
TJWRii <s1912558@ed.ac.uk>
2024-04-03 21:49:19 +0100

    TODO: fix prev info loading problem, elevation problem

--- account_files/schedule/AgendaScreen.js
+++ account_files/schedule/AgendaScreen.js
@@ -188,7 +188,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.23,
     shadowRadius: 2.62,
-    elevation: 4,
+    // elevation: 2,
   },
   itemText: {
     fontSize: 16, // Make text larger
